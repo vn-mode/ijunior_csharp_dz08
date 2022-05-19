a8faba895cee50d3267925755ceca998213ad9a6
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-05-19 03:14:28 +0300

    dz08
    dz08

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace vn_mode_csharp_dz08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int howManyTimes;
+
+            Console.Write("Напишите что-то приятное: ");
+            userInput = Console.ReadLine();
+            Console.Write($"Сколько раз вам вывести на экран '{userInput}': ");
+            howManyTimes = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < howManyTimes; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(userInput);
+            }
+        }
+    }
+}
